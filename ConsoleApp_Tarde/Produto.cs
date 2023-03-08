d7d8ff16f39a08855096fbcbcdd8455d07d40d62
STEFANI SOUZA <stefani.souza4@fatec.sp.gov.br>
2023-03-08 14:23:04 -0300

    Criei os atributos e o método costrutor

--- ConsoleApp_Tarde/Produto.cs
+++ ConsoleApp_Tarde/Produto.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_Tarde
 {
-    internal class Produto
+    public class Produto
     {
+        public Produto(string descricao, int id, decimal valor)
+        {
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
+        }
+        private int Id;
+        private string Descricao;
+        private decimal Valor;
+        public string getDescricao()
+        {
+            return "Descrição do produto: " + Descricao;
+        }
+        public string getValor()
+        {
+            return "Valor em R$ " + Valor;
+        }
+        public string getId()
+        {
+            return "Código:" + Id;
+        }
+
     }
 }
